712522d7981b742d8bafa6d9036fa2b38296ec58
heeryung <heeryung.c@gmail.com>
2016-04-19 01:03:06 +0900

    signin template

--- maingate.py
+++ maingate.py
@@ -1,14 +1,29 @@
 import os
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
-app = Flask('myapp', template_folder=tmpl_dir)
+moment = Moment(app)
+db = SQLAlchemy(app)
+
+    
+
 
 @app.route('/')
 def index():
@@ -19,4 +34,5 @@ def user(name):
     return render_template('user.html', name=name)
     
 if __name__ == '__main__':
+    db.create_all()
     manager.run()

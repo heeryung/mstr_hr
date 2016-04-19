49096932c7496b4f12aece119e50296e03284567
heeryung <heeryung.c@gmail.com>
2016-04-19 23:19:14 +0900

    latest before re-install

--- maingate.py
+++ maingate.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -7,6 +7,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
+
 
 
 #Database configuration
@@ -26,20 +27,20 @@ db = SQLAlchemy(app)
 # Role and User model definition
 class Role(db.Model):
     __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(64), unique = True)    
-    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
-       
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
     def __repr__(self):
         return '<Role %r>' % self.name
- 
+
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -49,13 +50,30 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
-    return render_template('./app/templates/index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('./app/templates/user.html', name=name)
+    return render_template('user.html', name=name)
     
 if __name__ == '__main__':
     db.create_all()
